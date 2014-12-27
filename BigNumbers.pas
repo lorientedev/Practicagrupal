6fb4d4db3949df44cd461571cf8af041b2dadc6c
Mariano Loriente <lorientedev@gmail.com>
2014-12-27 17:48:51 +0100

    Añado tipo t_keep_character y algunas variables
    t_keep character: Para almacenar numero leido

--- BigNumbers.pas
+++ BigNumbers.pas
@@ -1,12 +1,18 @@
 Program BigNumbers;
 
 type
-t_keep_numbers = array[1..50] of char;
-t_keeep_binary = array[1..200] of integer;
+t_keep_character_number = array[1..50] of char;
+t_keep_numbers = array[1..50]of integer;
+t_keep_binary = array[1..200] of integer;
 var 
+keep_character_number:t_keep_character_number;
 keep_numbers:t_keep_numbers;
 keep_binary: t_keep_binary;
-operation,number:char;
+operation:char;
+binary_lenght:integer;
+number_lenght:integer;
+
+
 
 BEGIN
 END.
